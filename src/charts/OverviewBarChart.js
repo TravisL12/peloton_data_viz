77b5537dfaf5a68d84cdaf998a21012d3a4582ae
Travis Lawrence <travis.lawrence12@gmail.com>
2022-12-23 12:44:11 -0800

    cleanup

--- src/charts/OverviewBarChart.js
+++ src/charts/OverviewBarChart.js
@@ -8,54 +8,54 @@ const OverviewBarChart = ({ data, colors, currentGraph, select }) => {
   const width = mainWidth - margin.left - margin.right;
   const height = mainHeight - margin.top - margin.bottom;
 
-  const updateBars = (selection) => {
-    return selection
-      .selectAll("rect")
-      .data((d) => d)
-      .join(
-        (enter) =>
-          enter
-            .append("rect")
-            .attr("x", (d) => xScale(d.data.date))
-            .attr("y", (d) => yScale(d[1]))
-            .attr("height", (d) => yScale(d[0]) - yScale(d[1]))
-            .attr("width", xScale.bandwidth()),
-        (update) => {
-          update
-            .transition()
-            .attr("x", (d) => xScale(d.data.date))
-            .attr("y", (d) => yScale(d[1]))
-            .attr("height", (d) => yScale(d[0]) - yScale(d[1]))
-            .attr("width", xScale.bandwidth());
-        }
-      );
-  };
-
   const allColors = colors[select.graphKey];
   const xScale = d3.scaleBand().range([0, width]).padding(0.3);
   const yScale = d3.scaleLinear().range([height, 0]);
 
+  const graphData = currentGraph.dataTransform(data, select.graphKey);
+  const groups = graphData.map((d) => d.date);
+  const stackGroup = getUniq(graphData.map((d) => d[select.graphKey]));
+  const changedData = graphData.map((d) => {
+    const output = stackGroup.reduce(
+      (acc, key) => {
+        acc[key] = 0;
+        return acc;
+      },
+      { date: d.date }
+    );
+    output[d[select.graphKey]] = d.value;
+    return output;
+  });
+  const stackedGen = d3.stack().keys(stackGroup)(changedData);
+
+  const updateBars = useCallback(
+    (selection) => {
+      return selection
+        .selectAll("rect")
+        .data((d) => d)
+        .join(
+          (enter) =>
+            enter
+              .append("rect")
+              .attr("x", (d) => xScale(d.data.date))
+              .attr("y", (d) => yScale(d[1]))
+              .attr("height", (d) => yScale(d[0]) - yScale(d[1]))
+              .attr("width", xScale.bandwidth()),
+          (update) => {
+            update
+              .transition()
+              .attr("x", (d) => xScale(d.data.date))
+              .attr("y", (d) => yScale(d[1]))
+              .attr("height", (d) => yScale(d[0]) - yScale(d[1]))
+              .attr("width", xScale.bandwidth());
+          }
+        );
+    },
+    [xScale, yScale]
+  );
+
   const drawGraph = useCallback(() => {
     const svg = d3.select(svgRef.current);
-
-    const graphData = currentGraph.dataTransform(data, select.graphKey);
-
-    // need object of each instructor with value associated
-    // {date: 1234, leanne: 25.3, bill: 10, ...} this makes subgropus work
-    const groups = graphData.map((d) => d.date);
-    const stackGroup = getUniq(graphData.map((d) => d[select.graphKey]));
-    const changedData = graphData.map((d) => {
-      const output = stackGroup.reduce(
-        (acc, key) => {
-          acc[key] = 0;
-          return acc;
-        },
-        { date: d.date }
-      );
-      output[d[select.graphKey]] = d.value;
-      return output;
-    });
-    const stackedGen = d3.stack().keys(stackGroup)(changedData);
 
     xScale.domain(groups);
     d3.select(`.x-axis`)
@@ -88,10 +88,10 @@ const OverviewBarChart = ({ data, colors, currentGraph, select }) => {
           updateBars(update.select(".bars"));
         },
         (exit) => {
-          exit.transition().duration(250).style("opacity", 0).remove();
+          exit.transition().style("opacity", 0).remove();
         }
       );
-  }, [colors, data, height, width, currentGraph, select]);
+  }, [allColors, graphData, groups, stackedGen, updateBars, xScale, yScale]);
 
   useEffect(() => {
     generateSvg(svgRef.current, height);
